5c3a9c522e7d65e0bf6f2c0fde3a7ed6d900ff5a
Richard Simões <rsimoes@cpan.org>
2012-02-09 15:50:22 +0000

    Import of RSIMOES/VM-Virtuozzo-v0.0.1-TRIAL from CPAN.
    gitpan-cpan-distribution: VM-Virtuozzo
    gitpan-cpan-version:      v0.0.1-TRIAL
    gitpan-cpan-path:         RSIMOES/VM-Virtuozzo-v0.0.1-TRIAL.tar.gz
    gitpan-cpan-author:       RSIMOES
    gitpan-cpan-maturity:     developer

---
+++ lib/VM/Virtuozzo.pm
@@ -0,0 +1,151 @@
+package VM::Virtuozzo;
+
+use v5.10;
+use strict;
+use warnings FATAL => "all";
+use utf8;
+use Carp;
+use File::ShareDir qw(dist_dir);
+use File::Spec::Functions qw(catdir catfile);
+use Hash::FieldHash qw(fieldhash);
+use IO::Socket::INET;
+use Params::Check "check";
+use POSIX "floor";
+use Socket qw(inet_aton);
+use XML::Compile::Cache;
+use VM::Virtuozzo::Response;
+
+use constant {
+	Document => "XML::LibXML::Document",
+	Element  => "XML::LibXML::Element",
+	Response => "VM::Virtuozzo::Response" };
+
+use namespace::clean;
+
+our $VERSION = 'v0.0.1'; # VERSION
+# ABSTRACT: Client implementation of the Parallels Virtuozzo XML API
+
+fieldhash my %client => "_client";
+fieldhash my %schema => "_schema";
+my $schema = XML::Compile::Cache->new(
+	allow_undeclared => 1,
+	opts_rw          => { elements_qualified => 0 } );
+do {
+	my $dist_dir  = dist_dir("VM-Virtuozzo");
+	my $xsd_dir   = catdir $dist_dir, "v4";
+	my @xsd_files = glob catfile $xsd_dir, "*.xsd";
+	$schema->importDefinitions($_) for @xsd_files; };
+
+sub new {
+	my ( $class, %params ) = @_;
+
+	# Check params:
+	my %tmpl = (
+		xsd_version => {
+			required => 1,
+			allow    => [4] }, # Only version supported for now
+		use_ssl => {
+			default => 1,
+			allow   => [ 0, 1 ] },
+		hostname => {
+			required => 1,
+			allow    => sub { $_[0] && inet_aton($_[0]) } } );
+	check( \%tmpl, \%params, 1 ) or croak "Parameter check failed";
+
+	# Create self and client attribute:
+	my $self = bless { }, $class;
+	$client{$self} = IO::Socket::INET->new(
+		PeerAddr => $params{hostname},
+		PeerPort => 4433,
+		Proto    => "tcp",
+		Timeout  => 10 ) or croak "Unable to connect to server";
+	local $/ = "\0";
+	$self->_client->getline;
+	$schema{$self} = $schema; # Schema accessor exists only for unit tests
+
+	return $self; }
+
+my $packet_id = 1;
+my $doc       = Document->new("1.0", "UTF-8");
+sub _write_packet {
+	my ($self, $namespace, $function, $params) = @_;
+	XML::Compile::Util->import(qw(pack_type));
+	my $protocol_ns = $namespace =~ s/\w+$/protocol/rx;
+	$protocol_ns =~ s/\bvza\b/vzl/xg;
+	my $packet_type =  pack_type($protocol_ns, "packet");
+	my $op_type = pack_type($namespace, $function);
+	my ($short_ns) = $namespace =~ /(\w+)$/x;
+	my $operator = Element->new($short_ns);
+
+	$operator->addChild(
+		defined($params)
+		? $self->_schema->writer($op_type)->($doc, $params)
+		: Element->new($function) );
+	my $packet = $self->_schema->writer($packet_type)->(
+		$doc, {
+			id => $packet_id++,
+			version => "4.0.0",
+			data => { cho_operator => [ { $short_ns => $operator } ] } } );
+	return $packet->toString; }
+
+# Generate API methods:
+
+foreach my $namespace ( $schema->namespaces->list ) {
+	my ($ns_short) = $namespace =~ /(\w+)$/x;
+	no strict "refs";
+	*{__PACKAGE__ . "::" . $ns_short} = sub {
+		use strict "refs";
+		my ($self, $function, $params) = @_;
+		my $operation = $self->_write_packet($namespace, $function, $params);
+		$operation =~ s/(\w+?>.+?==)\n/$1/gx;
+		$self->_client->print($operation . "\0");
+		local $/ = "\0";
+		return $self->_client->getline; } }
+#		return Response->new(
+#			$client->reader($namespace)->( $self->_client->getline ) ); } }
+
+1;
+
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+VM::Virtuozzo - Client implementation of the Parallels Virtuozzo XML API
+
+=head1 SYNOPSIS
+
+	my $vzzo = VM::Virtuozzo->new(
+		hostname    => "domain.tld",
+		use_ssl     => 0,
+		xsd_version => 4
+	);
+	$vzzo->system( login => {
+		name     => "root",
+		realm    => "00000000-0000-0000-0000-000000000000",
+		password => "mysecret123"
+	} );
+	$vzzo->vzaenvm( suspend => { # suspend a container
+		eid => "e43581cb-f13a-324d-aab5-e356e19ebee4"
+	} );
+
+=head1 DESCRIPTION
+
+c<VM::Virtuozzo> provides a client implementation of the Parallels Virtuozzo
+XML API, enabling one to remotely manage Parallels Virtuozzo Containers. The
+heavy lifting for this distribution is done by L<XML::Compile>.
+
+=head1 METHODS
+
+=head2
+
+=head1 AUTHOR
+
+Richard Simões C<< <rsimoes AT cpan DOT org> >>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright © 2012 Richard Simões. This module is released under the terms of the
+Artistic License 2.0 and may be modified and/or redistributed under the same or
+any compatible license.
---
+++ lib/VM/Virtuozzo/Response.pm
@@ -0,0 +1,14 @@
+package VM::Virtuozzo::Response;
+
+use v5.10;
+use strict;
+use warnings FATAL => "all";
+use utf8;
+use Carp;
+use Hash::FieldHash qw(fieldhash);
+use namespace::clean;
+
+our $VERSION = 'v0.0.1'; # VERSION
+# ABSTRACT: Parallels Virtuozzo API response class
+
+1;
